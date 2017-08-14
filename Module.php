d729283b413d9f23c60c9c8e2b1b9503e9dedf1a
dgarcia <garcia.gordo.david@gmail.com>
2017-08-14 13:01:36 +0200

    fix priority

--- Module.php
+++ Module.php
@@ -14,22 +14,31 @@ class Module
          * @var \Zend\ServiceManager\ServiceManager $sm
          * @var \Zend\Mvc\Application $application
          */
-        $eventManager = $e->getApplication()->getEventManager();
-        $eventManager->attach(MvcEvent::EVENT_ROUTE, array($this, 'configGedmoTranslations'), -100);
+        $application = $e->getApplication();
+        $eventManager = $application->getEventManager();
+        $translator = $application->getServiceManager()->get('translator');
+        $eventManager->attach(MvcEvent::EVENT_ROUTE, [$this, 'configGedmoTranslations'], -1000);
     }
 
     public function configGedmoTranslations(MvcEvent $e)
     {
         /**
          * @var \Zend\Mvc\Application $application
-         * @var \Zend\I18n\Translator\Translator $translator
+         * @var \Zend\Mvc\I18n\Translator $translator
          */
+
         $application = $e->getApplication();
+
+        $eventManager = $application->getEventManager();
         $translator = $application->getServiceManager()->get('translator');
         define('BELCEBUR_GEDMO_TRANSLATION_LOCALE', $translator->getLocale());
         define('BELCEBUR_GEDMO_TRANSLATION_FALLBACK_LOCALE', $translator->getFallbackLocale());
     }
 
+    public function getConfig(): array
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
 
     public function getAutoloaderConfig(): array
     {
@@ -40,12 +49,6 @@ class Module
                 ),
             ),
         );
-    }
-
-
-    public function getConfig(): array
-    {
-        return include __DIR__ . '/config/module.config.php';
     }
 
 
