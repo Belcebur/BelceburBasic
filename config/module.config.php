f07601388b6fb111559baef130d70f7b3b68d451
dgarcia <garcia.gordo.david@gmail.com>
2017-08-14 13:00:32 +0200

    update sintax

--- config/module.config.php
+++ config/module.config.php
@@ -1,25 +1,24 @@
 <?php
-return array(
-    'filters' => array(
-        'invokables' => array(
-            'nl2br' => 'BelceburBasic\Form\Filter\Nl2br',
-        ),
-    ),
-    'view_helpers'    => array(
-        'factories' => array(
+return [
+    'filters' => [
+        'invokables' => [
+            'nl2br' => \BelceburBasic\Form\Filter\Nl2br::class,
+        ],
+    ],
+    'view_helpers' => [
+        'factories' => [
             'bTools' => function (\Zend\View\HelperPluginManager $pluginManager) {
                 return new \BelceburBasic\View\Helper\BTools($pluginManager);
             },
-        ),
-    ),
-    'service_manager' => array(
-        'bAdmin' => 'BelceburBasic\Service\AdminNavigationFactory',
-    ),
-    'belcebur'        => array(
-        'belcebur-basic' => array(),
-    ),
-    'navigation'      => array(
-        'bAdmin'  => array(),
-        'default' => array(),
-    )
-);
+        ],
+    ],
+    'service_manager' => [
+        'bAdmin' => \BelceburBasic\Service\AdminNavigationFactory::class,
+    ],
+    'belcebur-basic' => [
+    ],
+    'navigation' => [
+        'bAdmin' => [],
+        'default' => [],
+    ]
+];
